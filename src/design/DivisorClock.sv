409f5a6966d91829b708cece27dbdada0d1a008e
Dennis Arce <dennisarce99@estudiantec.cr>
2024-11-17 14:04:12 -0600

    modificaciones 2

--- src/design/DivisorClock.sv
+++ src/design/DivisorClock.sv
@@ -3,10 +3,10 @@ module FrecDivider (
   output logic clk_div
 );
 
-    reg [26:0] cont = 0;
+    reg [26:0] cont = 27'd0;
 
     always @(posedge clk) begin
-        cont <= (cont >= 269) ? 0:(cont+1);
+        cont <= (cont >= 27'd269) ? 0:(cont+27'd1);
     end
 
     assign clk_div = (cont == 269) ? 1'b1:1'b0;
